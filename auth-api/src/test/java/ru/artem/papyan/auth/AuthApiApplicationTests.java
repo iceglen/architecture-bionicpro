de419de2e666c1df55c655d929117f219560b0d9
Artem Papyan <iceglen@yandex.ru>
2026-02-15 00:25:28 +0300

    task 1.2 skeleton

---
+++ auth-api/src/test/java/ru/artem/papyan/auth/AuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.artem.papyan.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
